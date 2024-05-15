3b956d56f9a899e4b72c6bb6e0e35e640f4aa89f
Mehedi <kpopdw@gmail.com>
2024-05-15 16:19:36 -0400

    hero: background video V2

--- src/components/HomeHelpers/Hero.jsx
+++ src/components/HomeHelpers/Hero.jsx
@@ -12,9 +12,25 @@ function Hero() {
   }, []);
   return (
     <Fragment>
-      <div className="w-full "></div>
-      <video src={myVideo} autoPlay loop muted className="object-none h-28 w-screen" />
-
+      <div className="w-full ">
+        <video src={myVideo} autoPlay loop muted className="object-none hidden md:h-svh md:block w-full opacity-85" />
+        <div className="text-container absolute w-2/3 text-black md:text-red-400 h-full top-1 left-0 flex lg:flex-row flex-col lg:items-center lg:justify-start">
+          <div className="max-w-wrapper mx-auto px-5 w-full pb-14 pt-10 xs:py-12 sm:py-20 lg:py-0 my-16 h-full lg:h-auto lg:block flex flex-col sm:justify-center justify-between">
+            <div className="h-auto">
+              <h1 className="pb-5 sm:w-auto flex flex-wrap lg:text-heading lg:leading-tight xl:leading-tighter
+            sm:text-5xl sm:leading-tight text-3xl text-2xl sm:max-w-header w-64 font-bold">
+                <span className="lg:w-full" data-aos="fade-up" data-aos-delay="100">Digital Mastery Unleashed.</span>
+              </h1>
+              <div className="pb-5 max-w-header mb-2 sm:mb-5">
+                <p className="text-xl sm:text-2xl lg:text-3xl font-light sm:flex flex-wrap ellipsis-p text-pretty sm:text-pretty ">
+                  <span className="inline lg:block" data-aos="fade-up" data-aos-delay="800">Embark on a journey of digital empowerment with us.</span>
+                  <span className="inline lg:block" data-aos="fade-up" data-aos-delay="850">Unlock the potential of your online presence today.</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
     </Fragment>
   );
