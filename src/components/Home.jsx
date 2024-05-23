be8a3f655c4d61a6402fcb3dbffad49ee8d4953d
Mehedi <kpopdw@gmail.com>
2024-05-23 15:17:09 -0400

    again somestuff changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,17 +10,18 @@ const Home = () => {
         <Fragment className="w-full h-full">
             <Hero />
             <div class="my-8 flex justify-center">
-                <hr class="w-1/2 border-t-4 border-indigo-500 rounded-lg shadow-lg" />
+                <hr class="xl:w-2/3 md:w-2/4  border-t-4 border-indigo-500 rounded-lg shadow-lg" />
             </div>
-            <div className="md:flex md:w-3/4 md:justify-center p-10 w-fit md:p-1 sm:h-30" >
-                <div className="md:invisible">
-                    <img src="src\assets\website.svg" alt="Laptop with lady" className='bottom-1' />
+            <div className="md:hidden">
+                <img src="src\assets\website.svg" alt="Laptop with lady" className='bottom-1' />
+            </div>
+            <div className="md:flex md:w-full xl:w-2/3 md:justify-center p-10 w-fit md:p-3 sm:h-30 sm:p-2 xl:pl-[20rem]" >
+
+                <div className="md:w-[55rem] md:h-[10rem] md:flex items-center justify-center content-center bg-purple-100 border-b-2 border-indigo-500 border-r-2  ">
+                    <span className='text-xl md:text-2xl' data-aos="fade-right" data-aos-delay="300 ">Smart Digital Solutions:</span>
+                    <h2 className="text-3xl md:text-4xl" data-aos="fade-right" data-aos-delay="400">For The Real World</h2>
                 </div>
-                <div className="md:w-1/3 flex items-center justify-center content-center bg-purple-100 border-b-2 border-indigo-500 border-r-2  ">
-                    <span className='text-xl md:text-2xl' data-aos="fade-right" data-aos-delay="800">Smart Digital Solutions:</span>
-                    <h2 className="text-3xl md:text-4xl" data-aos="fade-right" data-aos-delay="900">For The Real World</h2>
-                </div>
-                <div className='md:pl-4 md:pt-0 md:w-2/4'>
+                <div className='xl:pl-4 md:pt-0 md:w-fit pt-4 text-pretty font '>
                     <p>Step into the dynamic world of digital innovation, where creativity knows no bounds! From mesmerizing web design to groundbreaking marketing strategies, we're your gateway to online triumph. Let's ignite your digital journey!</p>
                 </div>
             </div>
