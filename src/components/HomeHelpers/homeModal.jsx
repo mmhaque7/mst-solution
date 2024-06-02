9fc4ce10b2f14e75fe3a7f37d2a1617192ae2cb0
mmhaque7 <mehedihaque1@gmail.com>
2024-06-01 23:30:52 -0400

    home still

--- src/components/HomeHelpers/homeModal.jsx
+++ src/components/HomeHelpers/homeModal.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 function HomeModal() {
   return (
-    <motion.div className="">
+    <motion.div className="m-0 p-0 box-border flex">
       <motion.h2>hello</motion.h2>
       <motion.div>
         <p>
@@ -14,6 +14,38 @@ function HomeModal() {
         </p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, aliquam.</p>
       </motion.div>
+
+      <motion.h2>hello</motion.h2>
+      <motion.div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+          officia illum ad ipsum voluptate molestias ratione quas, nulla
+          voluptatum modi?Z
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, aliquam.</p>
+      </motion.div>
+
+      <motion.h2>hello</motion.h2>
+      <motion.div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+          officia illum ad ipsum voluptate molestias ratione quas, nulla
+          voluptatum modi?Z
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, aliquam.</p>
+      </motion.div>
+
+      <motion.h2>hello</motion.h2>
+      <motion.div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+          officia illum ad ipsum voluptate molestias ratione quas, nulla
+          voluptatum modi?Z
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, aliquam.</p>
+      
+      </motion.div>
+
     </motion.div>
   );
 }
